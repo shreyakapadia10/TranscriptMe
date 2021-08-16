1f026f9be86b5d78f863acfa008622e17735cf3d
Shreya Kapadia <shreyakapadia8@gmail.com>
2021-08-16 18:31:47 +0530

    Completed email validation feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', views.register, name='Register'),
     path('login/', views.login, name='Login'),
     path('logout/', views.logout, name='Logout'),
+
+    path('activate/<uidb64>/<token>/', views.activate_account, name='ActivateAccount'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,21 @@ from django.contrib import messages
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
+# Email Verification Imports
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST, request.FILES)
 
         if form.is_valid():
+            # Getting form data
             first_name = form.cleaned_data.get('first_name')
             last_name = form.cleaned_data.get('last_name')
             email = form.cleaned_data.get('email')
@@ -19,13 +28,28 @@ def register(request):
             secret_id = form.cleaned_data.get('secret_id')
             profile_picture = 'default/user_avatar.png'
 
+            # Creating user
             user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, app_id=app_id, secret_id=secret_id, profile_picture=profile_picture, password=password)
 
-            user.is_active = True
+            user.is_active = False
             user.save()
 
-            messages.success(request=request, message='Account Created Successfully!')
-            return redirect('Login')
+            # Email Verification Process
+            current_site = get_current_site(request) # getting current site
+            mail_subject = 'Activate Your Account' # Setting mail subject
+            # Rendering html template and passing it necessary values
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+
+            to_email = email # email of the user
+            send_email = EmailMessage(mail_subject, message, to=[to_email]) # creating mail to be sent
+            send_email.send() # Sending mail
+
+            return redirect(f'/accounts/login/?command=verification&email={email}')
         else:
             messages.error(request, form.errors, form.errors)
     else:
@@ -60,3 +84,20 @@ def logout(request):
     messages.success(request, 'You are logged out now!')
     return redirect('Login')
 
+def activate_account(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode() # decoding the url
+        user = User._default_manager.get(pk=uid) # getting user from the given uid
+    except (TypeError, ValueError, User.DoesNotExist, OverflowError): # in case of invalid uid or user
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token): # if the link is valid
+        user.is_active = True # set is_active status of user to true, so that user can login
+        user.save() # saving user details
+
+        messages.success(request=request, message='Your account has been activated successfully! You can login now!')
+        
+    else:
+        messages.error(request, 'Invalid activation link!')
+
+    return redirect('Login')
