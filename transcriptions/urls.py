ea6a3c3ba4ba4b538266c99caa367a304588499d
Shreya Kapadia <shreyakapadia8@gmail.com>
2021-09-05 14:20:11 +0530

    Completed View Transcript History

--- transcriptions/urls.py
+++ transcriptions/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('text/', views.transcript_text, name='TranscriptText'),
     path('audio/', views.transcript_audio, name='TranscriptAudio'),
-    # path('download_files/', views.download_files, name='DownloadFiles'),
     path('download_files/<job_id>/<conversation_id>/', views.download_files, name='DownloadFiles'),
+    
+    path('history/', views.view_history, name='ViewHistory'),
 ]
